4aab2c76520cdfec21fd5266ef842f762c4e8045
Mauricio Selfene <m.selfene@gmail.com>
2021-08-21 09:05:34 -0400

    Incluye controllers  -> productos.routes.js, productos.js. modoles -> productos.js

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-// app.use(require('../controllers/productos/producto.routes'));
+app.use(require('../controllers/productos/productos.routes'));
 // app.use(require('../controllers/categorias/categoria.routes'));
 app.use(require("../controllers/usuarios/usuarios.routes"));
 
